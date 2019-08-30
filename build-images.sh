33770fef33224c9f0b46ed4dc2d797fc860d0462
Ryan <rynkrn321@gmail.com>
2019-08-29 22:01:59 -0400

    created scripts for building the images and pushing them to docker

---
+++ build-images.sh
@@ -0,0 +1,5 @@
+docker build --no-cache --pull -t rykroon/postgres:10 10/
+docker build --no-cache --pull -t rykroon/postgres:10-alpine 10/alpine/
+docker build --no-cache --pull -t rykroon/postgres:11 11/
+docker build --no-cache --pull -t rykroon/postgres:11-alpine 11/alpine/
+
---
+++ push-images.sh
@@ -0,0 +1,5 @@
+docker push rykroon/postgres:10
+docker push rykroon/postgres:10-alpine
+docker push rykroon/postgres:11
+docker push rykroon/postgres:11-alpine
+
